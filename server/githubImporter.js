1399031625e556837d61b49f86452a784c9491cb
ajwhatling-wk <aj.whatling@workiva.com>
2017-04-18 11:02:36 -0500

    Importing from github now returns a promise

--- server/githubImporter.js
+++ server/githubImporter.js
@@ -1,8 +1,12 @@
-let request = require('request-promise-native');
+let promiseBuilder = require('./utils/promiseBuilder'),
+  request = require('request-promise-native');
 
 function importGithubIssues(owner, repo) {
   let url = `https://api.github.com/repos/${owner}/${repo}/issues?status=open`;
-  request.get(url);
+
+  return promiseBuilder.create((resolve, reject) => {
+    request.get(url);
+  });
 }
 
 module.exports = {
--- test/server/githubImporter.spec.js
+++ test/server/githubImporter.spec.js
@@ -1,5 +1,6 @@
 let sinon = require('sinon'),
   expect = require('chai').expect,
+  Chance = require('chance'),
   request = require('request-promise-native'),
 
   logger = require('../../server/utils/logger'),
@@ -9,27 +10,40 @@ let sinon = require('sinon'),
 
 
 describe('Github Importer tests', () => {
-  let sandbox;
+  let sandbox,
+    chance,
+    owner,
+    repo,
+    fakePromise;
 
   beforeEach('set up', () => {
     sandbox = sinon.sandbox.create();
+    chance = new Chance();
 
+    owner = chance.word();
+    repo = chance.word();
+
+    fakePromise = 'this is a promise ' + chance.string();
     sandbox.stub(request, 'get');
+    sandbox.stub(promiseBuilder, 'create').returns(fakePromise);
   });
 
   afterEach(() => sandbox.restore());
 
-  it('should perform a GET request to fetch the open issues for the given repo and repo owner', () => {
-    let repo_owner = 'repo-owner',
-      repo = 'repo';
+  it('should return a promise', () => {
+    let issuesPromise = importGithubIssues(owner, repo);
 
-    importGithubIssues(repo_owner, repo);
-
-    sinon.assert.calledOnce(request.get);
-    sinon.assert.calledWithExactly(request.get, `https://api.github.com/repos/${repo_owner}/${repo}/issues?status=open`);
+    expect(issuesPromise).to.equal(fakePromise);
   });
 
-  it('should convert the json to a list of rows with the issue title and user', () => {
+  it('should perform a GET request to fetch the open issues for the given repo and repo owner', () => {
+    importGithubIssues(owner, repo);
 
+    sinon.assert.notCalled(request.get);
+
+    promiseBuilder.create.getCall(0).args[0]();
+
+    sinon.assert.calledOnce(request.get);
+    sinon.assert.calledWithExactly(request.get, `https://api.github.com/repos/${owner}/${repo}/issues?status=open`);
   });
 });
