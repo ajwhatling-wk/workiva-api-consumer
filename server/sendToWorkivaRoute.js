8bac5b19bbb59ab5ec9b6015690edf5fec86f724
ajwhatling-wk <aj.whatling@workiva.com>
2017-04-18 13:56:28 -0500

    Move exporting to workiva after importing from github

--- server/sendToWorkivaRoute.js
+++ server/sendToWorkivaRoute.js
@@ -1,25 +1,34 @@
 let logger = require('./utils/logger'),
-  workivaExporter = require('./workivaExporter');
+  workivaExporter = require('./workivaExporter'),
+  githubImporter = require('./githubImporter');
 
 module.exports = {
   route: '/sendToWorkiva',
   handler: (req, res) => {
     logger.log('About to do export to Workiva');
-
-    workivaExporter
-      .exportData({
-        apiUrl: process.env.WORKIVA_API_URL,
-        authToken: process.env.WORKIVA_AUTH_TOKEN,
-        dataToSave: req.body
+    
+    githubImporter
+      .importGithubIssues(req.body.github_owner, req.body.github_repo)
+      .then(data => {
+        workivaExporter
+          .exportData({
+            apiUrl: process.env.WORKIVA_API_URL,
+            authToken: process.env.WORKIVA_AUTH_TOKEN,
+            dataToSave: data
+          })
+          .then(() => {
+            logger.log('Export succeeded');
+            res.send('');
+          })
+          .catch(() => {
+            logger.log('Export failed');
+            res.status(400);
+            res.send('failed');
+          });
       })
-      .then(() => {
-        logger.log('Export succeeded');
-        res.send('');
-      })
-      .catch(() => {
-        logger.log('Export failed');
+      .catch(err => {
         res.status(400);
-        res.send('failed');
+        res.send(err);
       });
   }
 };
--- test/server/sendToWorkivaRoute.spec.js
+++ test/server/sendToWorkivaRoute.spec.js
@@ -2,6 +2,7 @@ let sinon = require('sinon'),
   expect = require('chai').expect,
   Chance = require('chance'),
   logger = require('../../server/utils/logger'),
+  githubImporter = require('../../server/githubImporter'),
   workivaExporter = require('../../server/workivaExporter'),
   route = require('../../server/sendToWorkivaRoute');
 
@@ -11,6 +12,7 @@ describe('SendToWorkiva Route Tests', () => {
 
     fakeCatchPromise,
     fakePromise,
+    fakeGithubPromise,
     fakeRequest,
     fakeResponse,
     fakeExportData;
@@ -29,9 +31,18 @@ describe('SendToWorkiva Route Tests', () => {
       then: sandbox.stub().returns(fakeCatchPromise)
     };
 
-    fakeExportData = sandbox.stub(workivaExporter, 'exportData');
+    fakeGithubPromise = {
+      then: sandbox.stub(),
+      catch: sandbox.stub()
+    };
 
+    fakeGithubPromise.then.returns(fakeGithubPromise);
+    fakeGithubPromise.catch.returns(fakeGithubPromise);
+
+    fakeExportData = sandbox.stub(workivaExporter, 'exportData');
     fakeExportData.returns(fakePromise);
+
+    sandbox.stub(githubImporter, 'importGithubIssues').returns(fakeGithubPromise);
 
     fakeRequest = {
       body: {foo: chance.string()}
@@ -58,8 +69,48 @@ describe('SendToWorkiva Route Tests', () => {
     expect(route.handler).to.be.a('function');
   });
 
-  it('should call exportData on workivaExporter', () => {
+  it('should call importGithubIssues first to fetch github data', () => {
+    fakeRequest.body.github_owner = chance.name();
+    fakeRequest.body.github_repo = chance.word();
+
     route.handler(fakeRequest, fakeResponse);
+
+    sinon.assert.calledOnce(githubImporter.importGithubIssues);
+    sinon.assert.calledWithExactly(
+      githubImporter.importGithubIssues,
+      fakeRequest.body.github_owner,
+      fakeRequest.body.github_repo
+    );
+  });
+
+  it('should not call exportData on workivaExporter when github import fails', () => {
+    route.handler(fakeRequest, fakeResponse);
+
+    let error = 'failed';
+    fakeGithubPromise.catch.getCall(0).args[0](error);
+
+    sinon.assert.calledOnce(fakeGithubPromise.catch);
+    sinon.assert.notCalled(workivaExporter.exportData);
+  });
+
+  it('should respond with status 400 and send the error when github import fails', () => {
+    route.handler(fakeRequest, fakeResponse);
+
+    let error = 'failed';
+    fakeGithubPromise.catch.getCall(0).args[0](error);
+    
+    sinon.assert.calledOnce(fakeResponse.status);
+    sinon.assert.calledWithExactly(fakeResponse.status, 400);
+    
+    sinon.assert.calledOnce(fakeResponse.send);
+    sinon.assert.calledWithExactly(fakeResponse.send, error);
+  });
+
+  it('should call exportData on workivaExporter when github import is successful', () => {
+    route.handler(fakeRequest, fakeResponse);
+
+    let data = {};
+    fakeGithubPromise.then.getCall(0).args[0](data);
 
     sinon.assert.calledOnce(workivaExporter.exportData);
   });
@@ -67,15 +118,21 @@ describe('SendToWorkiva Route Tests', () => {
   it('should call exportData on workivaExporter with the correct parameters', () => {
     route.handler(fakeRequest, fakeResponse);
 
+    let data = [[chance.name(), chance.word()]];
+    fakeGithubPromise.then.getCall(0).args[0](data);
+
     sinon.assert.calledWithExactly(workivaExporter.exportData, {
       apiUrl: process.env.WORKIVA_API_URL,
       authToken: process.env.WORKIVA_AUTH_TOKEN,
-      dataToSave: fakeRequest.body
+      dataToSave: data
     });
   });
 
   it('should call exportData and when export succeeds it should call response.send()', () => {
-    route.handler({}, fakeResponse);
+    route.handler(fakeRequest, fakeResponse);
+
+    let data = [[chance.name(), chance.word()]];
+    fakeGithubPromise.then.getCall(0).args[0](data);
 
     let thenCallback = fakePromise.then.getCall(0).args[0];
 
@@ -86,7 +143,10 @@ describe('SendToWorkiva Route Tests', () => {
   });
 
   it('should call exportData and when export fails it should call response.status(400).send("failed")', () => {
-    route.handler({}, fakeResponse);
+    route.handler(fakeRequest, fakeResponse);
+
+    let data = [[chance.name(), chance.word()]];
+    fakeGithubPromise.then.getCall(0).args[0](data);
 
     let catchCallback = fakeCatchPromise.catch.getCall(0).args[0];
 
